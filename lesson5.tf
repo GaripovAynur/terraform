5a25dac953fa09a21dfea2658f3cbc0eb409ac4a
GaripovAynur <kuyan911@gmail.com>
2021-03-18 10:49:02 +0300

    Update lesson5.tf

--- lesson5.tf
+++ lesson5.tf
@@ -8,4 +8,9 @@ resource "aws_instance" "my_Ubuntu" {
   count         = 3
   ami           = "ami-08962a4068733a2b6"
   instance_type = "t3.micro"
+  tegs = {
+    name = "My Ubuntu Server"
+    Owner = "Aynur Garipov"
+    Project = "terraform learn"
+  }
 }
