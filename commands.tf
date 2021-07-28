5da4328e7d5fdabb9313fc0a8116fa9dc05ddf9d
GaripovAynur <kuyan911@gmail.com>
2021-07-28 14:48:59 +0300

    Update commands.tf

--- commands.tf
+++ commands.tf
@@ -3,6 +3,9 @@ terraform plan  #Пройдется по всей директории, посм
 terraform apply #Создат то что указали в файлах
 terraform destroy #Удалить все инсстансы что описано *.tf
 
+terraform console
+> templatefile("user_data.sh.tpl", { f_name = "Denis", l_name = "Astahov",  names  = ["Vasya", "Kolya", "Petya", "John", "Donald", "Masha"] }) # Для проверки без деплоя, показывает что будет делать данная команда.
+
 ##############На каждой сессии необходимо выполнять#########################################
 export AWS_ACCESS_KEY_ID = gHDFUImbfd43gb3t59450 #Чтобы *.tf не записывать данные по подключению, можно сделать так
 expert AWS_SECRET_ACCESS_KEY = fjdsife98BTYDTydvfhdsregter4 #Чтобы *.tf не записывать данные по подключению, можно сделать так
