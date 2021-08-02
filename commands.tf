67bbb0737308aca81d05238d0117577074a64357
GaripovAynur <kuyan911@gmail.com>
2021-08-02 16:30:09 +0300

    Update commands.tf

--- commands.tf
+++ commands.tf
@@ -10,3 +10,18 @@ terraform console
 export AWS_ACCESS_KEY_ID = gHDFUImbfd43gb3t59450 #Чтобы *.tf не записывать данные по подключению, можно сделать так
 expert AWS_SECRET_ACCESS_KEY = fjdsife98BTYDTydvfhdsregter4 #Чтобы *.tf не записывать данные по подключению, можно сделать так
 expert AWS_DEFAULT_REGION = eu-central-1 #Тоже самое
+
+
+####### SSH #######
+resource "aws_key_pair" "alice" {
+  key_name   = "alice"
+  public_key = file("./keys/ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "alice" {
+  key_name      = aws_key_pair.alice.key_name
+  ami           = "ami-03a71cec707bfc3d7"
+  instance_type = "t3.micro"
+}
+
+
