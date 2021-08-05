7676269ebd0fa70eeedb99ae25782a042788f2cd
Aynur <kuyan911@gmail.com>
2021-08-05 11:37:01 +0300

    08-05-21

--- terraform-lessons/Lesson-07/outputs.tf
+++ terraform-lessons/Lesson-07/outputs.tf
@@ -13,6 +13,7 @@ output "my_instance_arn" {
   value       = aws_instance.my_webserver.arn
 }
 
+
 output "my_sg_id" {
   description = "SecurityGroup of our WebSite"
   value       = aws_security_group.my_webserver.id
